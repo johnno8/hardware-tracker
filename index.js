8fc82f9edc37f5a44d1fdb43d6582c9cbc76e338
john o'keeffe <jokeeffe09@gmail.com>
2017-06-22 16:11:28 +0100

    Initial data loaded from fixtures.json

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const Vision = require('vision')
 const Inert = require('inert')
 // const models = require('./app/models')
 const db = require('./app/models/index.js')
+const SequelizeFixtures = require('sequelize-fixtures')
 
 const server = new Hapi.Server()
 
@@ -37,11 +38,23 @@ server.register([Vision, Inert], (err) => {
 
   // models.sequelize.sync({ force: true }).then(() => {
   db.sequelize.sync({ force: true }).then(() => {
-    server.start((err) => {
-      if (err) {
-        throw err
-      }
-      console.log(`Server running at: ${server.info.uri}`)
+
+    //load fixtures
+    SequelizeFixtures.loadFile('fixtures/*.json', db).then(function () {
+      console.log("DEV DATA CREATED SUCCESSFULLY")
+      server.start((err) => {
+        if (err) {
+          throw err
+        }
+        console.log(`Server running at: ${server.info.uri}`)
+      })
     })
+
+    // server.start((err) => {
+    //   if (err) {
+    //     throw err
+    //   }
+    //   console.log(`Server running at: ${server.info.uri}`)
+    // })
   })
 })
