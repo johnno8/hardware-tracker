1fcb5612e41e1eae6ef99a19912436a0d9c6d575
john o'keeffe <jokeeffe09@gmail.com>
2017-07-17 09:58:45 +0100

    Delete device button now renders appropriately

--- app/views/helpers/action.js
+++ app/views/helpers/action.js
@@ -15,21 +15,23 @@ function action (context, showDelete) {
         '<input name="description" type="hidden" value="' + context.description + '">' +
         '<button class="ui fluid red submit button">Delete</button>' +
         '</form>'
-  } else if (context.EmployeeId && !showDelete) {
-    return '' + '<form action="/release" method="POST">' +
-        '<input name="serial_num" type="hidden" value="' + context.serial_num + '">' +
-        '<input name="EmployeeId" type="hidden" value="' + context.EmployeeId + '">' +
-        '<button class="ui fluid grey submit button">Release</button>' +
-        '</form>'
-  } else { //(!context.EmployeeId && !showDelete){
-    return '' + '<form action="/assignment" method="POST">' +
-        '<input name="serial_num" type="hidden" value="' + context.serial_num + '">' +
-        '<input name="type" type="hidden" value="' + context.type + '">' +
-        '<input name="make" type="hidden" value="' + context.make + '">' +
-        '<input name="model" type="hidden" value="' + context.model + '">' +
-        '<input name="description" type="hidden" value="' + context.description + '">' +
-        '<button class="ui fluid blue submit button">Assign</button>' +
-        '</form>'
+  } else {
+    if(context.EmployeeId){
+      return '' + '<form action="/release" method="POST">' +
+            '<input name="serial_num" type="hidden" value="' + context.serial_num + '">' +
+            '<input name="EmployeeId" type="hidden" value="' + context.EmployeeId + '">' +
+            '<button class="ui fluid grey submit button">Release</button>' +
+            '</form>'
+    } else {
+      return '' + '<form action="/assignment" method="POST">' +
+            '<input name="serial_num" type="hidden" value="' + context.serial_num + '">' +
+            '<input name="type" type="hidden" value="' + context.type + '">' +
+            '<input name="make" type="hidden" value="' + context.make + '">' +
+            '<input name="model" type="hidden" value="' + context.model + '">' +
+            '<input name="description" type="hidden" value="' + context.description + '">' +
+            '<button class="ui fluid blue submit button">Assign</button>' +
+            '</form>'
+    }
   }
 }
 
